22f709f64aa860a28856bb40f2a11e5332d7de87
Kalpesh Senva <shenvakalpesh4@gmail.com>
2023-05-29 18:06:13 +0530

    second commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const authRoute=require("./routes/auth")
 const userRoute=require("./routes/users")
 const postRoute=require("./routes/posts")
+const categoryRoute=require("./routes/categories")
+const multer=require("./Images")
 
 dotenv.config()
 app.use(express.json())
@@ -17,9 +19,24 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(console.log("connected to MongoDB")).catch((err)=>console.log(err));
 
 
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"Images")
+    },
+    filename:(req,file,cb)=>{
+        cb(null,req.body.name)
+    }
+})
+
+const upload=multer({storage:storage});
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    res.status(200).json("File has been Uploaded")
+})
+
 app.use('/api/auth',authRoute)
 app.use('/api/users',userRoute)
 app.use("/api/posts",postRoute)
+app.use("/api/categories",categoryRoute)
 app.listen("5000", () => {
     console.log("Backend is Runing..")
 })
